d00fa0b9d33f83ca6948d67a1ef9c0df5d16f6a0
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-02-20 17:14:33 -0500

    feat: :sparkles: add custom error handler with conditional stack trace

---
+++ src/middleware/on-error.ts
@@ -0,0 +1,25 @@
+import type { ErrorHandler } from "hono";
+import type { ContentfulStatusCode, StatusCode } from "hono/utils/http-status";
+
+import { INTERNAL_SERVER_ERROR, OK } from "@/constants/http-status-codes";
+
+export const onError: ErrorHandler = (err, c) => {
+  const currentStatus = "status" in err
+    ? err.status as StatusCode
+    : c.res.status;
+
+  const statusCode = currentStatus !== OK
+    ? currentStatus
+    : INTERNAL_SERVER_ERROR;
+
+  // Usar solo c.env para entornos
+  const isProd = c.env && c.env.NODE_ENV === "production";
+
+  return c.json(
+    {
+      message: err.message,
+      stack: isProd ? undefined : err.stack,
+    },
+    statusCode as ContentfulStatusCode,
+  );
+};
