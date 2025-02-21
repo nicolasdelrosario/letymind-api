310019de3a7bfe061ba4a69ae968be30cbdf2dd6
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-02-21 13:09:56 -0500

    feat: :sparkles: add json content helper to structure json content with zod schema

---
+++ src/helpers/json-content.ts
@@ -0,0 +1,14 @@
+import type { ZodSchema } from "@/helpers/types";
+
+export function jsonContent<
+  T extends ZodSchema,
+>(schema: T, description: string) {
+  return {
+    content: {
+      "application/json": {
+        schema,
+      },
+    },
+    description,
+  };
+}
