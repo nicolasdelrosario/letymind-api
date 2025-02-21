4648141a1419289305f13248e6c86a8bc4ffdca2
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-02-21 12:41:21 -0500

    feat: :sparkles: added default hook to OpenAPI router configuration

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -1,15 +1,17 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { logger } from "hono/logger";
 
+import type { AppBindings } from "@/lib/types";
+
+import { defaultHook } from "@/middleware/default-hook";
 import { notFound } from "@/middleware/not-found";
 import { onError } from "@/middleware/on-error";
 import { serveEmojiFavicon } from "@/middleware/serve-emoji-favicon";
 
-import type { AppBindings } from "./types";
-
 export function createRouter() {
   return new OpenAPIHono<{ Bindings: AppBindings }>({
     strict: false,
+    defaultHook,
   });
 }
 
