bffb08899e6489e95e90eb03ae13a3f848d2f4ac
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-02-21 17:02:46 -0500

    feat: :label: add AppRouteHandler type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,13 @@
-import type { OpenAPIHono } from "@hono/zod-openapi";
+import type { OpenAPIHono, RouteConfig, RouteHandler } from "@hono/zod-openapi";
 
 export interface AppBindings {
   DB: D1Database;
 }
 
+export interface Env {
+  Bindings: AppBindings;
+}
+
 export type AppOpenApi = OpenAPIHono<{ Bindings: AppBindings }>;
+
+export type AppRouteHandler<R extends RouteConfig> = RouteHandler<R, Env>;
