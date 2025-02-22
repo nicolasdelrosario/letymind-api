513edbf157cd2006cf827579a8e4991103f77165
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-02-22 00:05:20 -0500

    chore: :sparkles: add drizzle basic configuration

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "sqlite",
+  driver: "d1-http",
+  schema: "src/db/schema.ts",
+  out: "drizzle/migrations",
+  // dbCredentials needs only for connect drizzle studio 🫠
+  // dbCredentials: {
+  //   accountId: "acc id token",
+  //   databaseId: "db id to connect",
+  //   token: "your access token from CF",
+  // },
+});
