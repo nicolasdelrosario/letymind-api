8d14add9668f8a4588b349a72185b96fb0c720b5
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-02-26 22:13:20 -0500

    feat: :sparkles: add capitalize words function

---
+++ src/utils/string.ts
@@ -0,0 +1,8 @@
+export function capitalizeWords(str: string): string {
+  if (!str)
+    return str;
+  return str
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ");
+}
