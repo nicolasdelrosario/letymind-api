bfc4105100eb9f3f82dd8edeb57ff883164588db
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-02-27 14:31:09 -0500

    feat: :sparkles: add created_at and updated_at properties

--- src/db/schema/seniorities.ts
+++ src/db/schema/seniorities.ts
@@ -1,3 +1,4 @@
+import { sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
@@ -6,6 +7,8 @@ export const seniorities = sqliteTable("seniorities", {
   id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
   name: text("name").notNull().unique(),
   description: text("description"),
+  createdAt: text("created_at").notNull().default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: text("updated_at").notNull().default(sql`CURRENT_TIMESTAMP`).$onUpdate(() => sql`CURRENT_TIMESTAMP`),
 });
 
 export const selectedSenioritiesSchema = createSelectSchema(seniorities).extend({
@@ -17,6 +20,8 @@ export const insertSenioritySchema = createInsertSchema(seniorities).extend({
   description: z.string().min(10).max(500).optional(),
 }).omit({
   id: true,
+  createdAt: true,
+  updatedAt: true,
 });
 
 export const patchSenioritySchema = insertSenioritySchema.partial();
